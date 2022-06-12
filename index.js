65a5bb2a3654569d42d770396dbb4cd9ddaefddb
rushracer <107320475+liltable@users.noreply.github.com>
2022-06-12 19:49:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, Collection } = require("discord.js");
+const client = new Client({intents: 32767});
+const { keepalive } = require("./keepalive.js")
+
+
+client.commands = new Collection();
+client.filters = new Collection();
+client.filterLog = new Collection();
+client.voiceGenerator = new Collection();
+
+module.exports = client;
+
+/*
+require('./Structures/Handlers/anticrash')(client);
+require('./Structures/Handlers/events')(client);
+require('./Structures/Handlers/commands')(client);
+*/
+
+client.login(proces.env.token)
